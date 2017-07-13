c47e8c5dd7a614b34b5ec9922d37d532991c1050
pablorodrigo <wfpablo@hotmail.com>
2017-07-13 08:53:56 -0400

    adicionando sripte do personagem e script da movimentaçao pra esquerda ou direita

---
+++ projetos/AcePanic/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D playerRb;
+	private SpriteRenderer playerSprite;
+	public float velocidade;
+	public bool flipX;
+
+	// Use this for initialization
+	void Start () {
+		playerRb = GetComponent<Rigidbody2D> ();
+		playerSprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// mudar velocidade e direção da movimentação
+		if (Input.GetMouseButtonDown (0)) {
+			velocidade *= -1;
+			flipX = !flipX;
+			playerSprite.flipX = flipX;
+		}
+
+		playerRb.velocity = new Vector2 (velocidade,playerRb.velocity.y);
+	}
+}
